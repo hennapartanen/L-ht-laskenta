3650a0dd201368a58ebafa1f1b412a49ccebcc8b
Henna Partanen <henna.partanen@itvelhot.local>
2020-11-10 10:42:47 +0200

    taustakuvie vaihtoa

--- js/background.js
+++ js/background.js
@@ -1,35 +1,35 @@
 
 
 
-function showpicc(clicked_id) {
+function showpicc() {
 
     document.getElementById('chrish').style.border = "3px solid black";
     document.getElementById('chris').style.display='block';
-    console.log(clicked_id);
+    
 }
 
 
-function showpice(clicked_id) {
+function showpice() {
 
     document.getElementById('emth').style.border = "3px solid black";
     document.getElementById('emt').style.display='block';
-    console.log(clicked_id);
+   
 }
 
 
-function showpicn(clicked_id) {
+function showpicn() {
 
     document.getElementById('newh').style.border = "3px solid black";
     document.getElementById('new').style.display='block';
-    console.log(clicked_id);
+   
 }
 
 
-function showpicv(clicked_id) {
+function showpicv() {
 
     document.getElementById('vach').style.border = "3px solid black";
     document.getElementById('vac').style.display='block';
-    console.log(clicked_id);
+    
 }
 
 
